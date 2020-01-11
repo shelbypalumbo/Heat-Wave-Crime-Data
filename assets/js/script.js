b0f933710876bc8da26ead131a4b4a030861f3c8
shelby palumbo <shelbypalumbo94@gmail.com>
2020-01-11 13:26:11 -0500

    Added geolocator to map.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,24 @@
 
-$(document).ready(function() {
+$(document).ready(function () {
     mapboxgl.accessToken = 'pk.eyJ1IjoibGF3bmExMiIsImEiOiJjazU3ZXdrcjYwMzVuM2VtN25wanM5eGh4In0.tVXQj-3Lrl58e2SmJgyjmw';
-    
+
     var map = new mapboxgl.Map({
         container: 'map', // container id
         style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
         center: [-74.5, 40], // starting position [lng, lat]
         zoom: 9 // starting zoom
-        
+
     });
-    
-    
+
+
+    var geocoder = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+    });
+
+    var geolocator = $('#geocoder');
+    geolocator.append(geocoder.onAdd(map));
+    $('#map').append(geolocator);
 
 
 
