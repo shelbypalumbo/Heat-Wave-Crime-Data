0d082db8c94b30d5a93aff4b44281013a24fe940
Patrick Dunn <pmdunn78@gmail.com>
2020-01-11 13:59:36 -0500

    merging files

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
         container: 'map', // container id
         style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
         center: [-74.5, 40], // starting position [lng, lat]
-        zoom: 9 // starting zoom
+        zoom: 18 // starting zoom
         
     });
     
