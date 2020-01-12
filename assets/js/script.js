31ac87333acbc46f3cd684cd7bac7a07e5cd70e6
Patrick Dunn <pmdunn78@gmail.com>
2020-01-12 16:31:02 -0500

    using openweatherAPI for initial load

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,38 @@
-
 $(document).ready(function () {
-    mapboxgl.accessToken = 'pk.eyJ1IjoibGF3bmExMiIsImEiOiJjazU3ZXdrcjYwMzVuM2VtN25wanM5eGh4In0.tVXQj-3Lrl58e2SmJgyjmw';
+    
+    var getCity = function(place) {
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + place + "&units=imperial" + "&apikey=4d721e459b51eed9d4d8047d079984e6";
 
-    var map = new mapboxgl.Map({
-        container: 'map', // container id
-        style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-        center: [-74.5, 40], // starting position [lng, lat]
-        zoom: 9 // starting zoom
+        $.ajax({
+            url:queryURL,
+            method:"GET"
+        }).then(function(response) {
+            console.log(response);
+            var latitude = response.coord.lat;
+            var longitude = response.coord.lon;
 
-    });
+            mapboxgl.accessToken = 'pk.eyJ1IjoibGF3bmExMiIsImEiOiJjazU3ZXdrcjYwMzVuM2VtN25wanM5eGh4In0.tVXQj-3Lrl58e2SmJgyjmw';
+            
+            var map = new mapboxgl.Map({
+                container: 'map', // container id
+                style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+                center: [longitude, latitude], // starting position [lng, lat]
+                zoom: 12 // starting zoom
+            });
+            var geocoder = new MapboxGeocoder({
+                accessToken: mapboxgl.accessToken,
+                mapboxgl: mapboxgl
+            });
 
+            var geolocator = $('#geocoder');
+            geolocator.append(geocoder.onAdd(map));
+            $('#map').append(geolocator);
 
-    var geocoder = new MapboxGeocoder({
-        accessToken: mapboxgl.accessToken,
-        mapboxgl: mapboxgl
-    });
+            map
+        });
+    };
 
-    var geolocator = $('#geocoder');
-    geolocator.append(geocoder.onAdd(map));
-    $('#map').append(geolocator);
-
-
+    getCity("Manhattan");
 
 
 });
