713fe5a1f46e3d0be108cd776ac2f3650486cc96
Patrick Dunn <pmdunn78@gmail.com>
2020-01-14 12:19:23 -0500

    changing map based on user search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,25 @@
 $(document).ready(function () {
+    // initialize global variables
+    var input = "";
     
+    // on-click to make map appear
+    $(".success").on("click", function() {
+        input = $("#city-input").val().trim();
+        getCity(input);
+    });
+
+    // pressing 'enter' key to make map appear
+    $("#city-input").keyup(function(event) {
+        var code = event.which;
+        input = $("#city-input").val().trim();
+        if (code == 13) {
+            event.preventDefault();
+            getCity(input);
+        }
+    });
+
+    // this function takes a location input and makes a call to the openweathermap API...
+    // ...then uses the returned latitude & longitude as inputs for the initilization of the map from mapbox
     var getCity = function(place) {
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + place + "&units=imperial" + "&apikey=4d721e459b51eed9d4d8047d079984e6";
 
@@ -11,28 +31,35 @@ $(document).ready(function () {
             var latitude = response.coord.lat;
             var longitude = response.coord.lon;
 
+            //  mapbox access token
             mapboxgl.accessToken = 'pk.eyJ1IjoibGF3bmExMiIsImEiOiJjazU3ZXdrcjYwMzVuM2VtN25wanM5eGh4In0.tVXQj-3Lrl58e2SmJgyjmw';
             
+            // mapbox API call
             var map = new mapboxgl.Map({
                 container: 'map', // container id
                 style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
                 center: [longitude, latitude], // starting position [lng, lat]
                 zoom: 12 // starting zoom
             });
-            var geocoder = new MapboxGeocoder({
-                accessToken: mapboxgl.accessToken,
-                mapboxgl: mapboxgl
-            });
 
-            var geolocator = $('#geocoder');
-            geolocator.append(geocoder.onAdd(map));
-            $('#map').append(geolocator);
+            // adds mapbox searchbar using our access token
+            // var geocoder = new MapboxGeocoder({
+            //     accessToken: mapboxgl.accessToken,
+            //     mapboxgl: mapboxgl
+            // });
 
+            // // appends mapbox searchbar to the map
+            
+            // var geolocator = $('#geocoder');
+            // geolocator.append(geocoder.onAdd(map));
+            // $('#map').append(geolocator);
+
+            // calls the mapbox API
             map
         });
     };
 
-    getCity("Manhattan");
+    
 
 
 });
